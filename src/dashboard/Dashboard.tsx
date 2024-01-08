2e793bba33d7f8b3f417b795a67dd2738bf06883
nemo-omen <nemo.omen@gmail.com>
2024-01-08 13:37:51 -0600

    Add icons

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { main } from 'bun';
 import { Context } from 'hono'
 import { FC } from 'hono/jsx';
+import { Icon } from '../common/components/Icon';
 
 export const Dashboard = (c: Context) => {
   return c.render(
@@ -17,11 +18,12 @@ export const Dashboard = (c: Context) => {
 const FeedSidebar: FC = () => {
   return(
     <aside class="sidebar" id="sidebar-main">
-      <nav aria-label="feed-sidebar">
-
+      <nav aria-label="feed-sidebar" id="#sidebar-nav">
       <ul>
-        <li><a href="/home">All Feeds</a></li>
-        <li><a href="/home/notes">Notes</a></li>
+        <li>
+          <a href="/home" class="icon-link"><Icon name="list" />All Posts</a>
+        </li>
+        <li><a href="/home/notes" class="icon-link"><Icon name="inbox" />Unread</a></li>
       </ul>
       </nav>
     </aside>
